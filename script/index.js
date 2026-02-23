dce207a9c66cfefe9710e5e86f9c879489b27c3d
Joti <hasnahenajoti1414@gmail.com>
2026-02-23 16:22:17 +0600

    added toggle style on  filtered buttons

--- script/index.js
+++ script/index.js
@@ -4,8 +4,34 @@ const rejectedCount = document.getElementById('total-rejected');
 
 const allCardsSection = document.getElementById('all-cards');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
 function calculateCount(){
     totalJobs.innerText = allCardsSection.children.length;
 }
 
 calculateCount()
+
+function toggleStyle(id) {
+    
+    // remove btn-active from all filter buttons
+    allFilterBtn.classList.remove('btn-active');
+    interviewFilterBtn.classList.remove('btn-active');
+    rejectedFilterBtn.classList.remove('btn-active');
+    
+    // adding btn-active only to the clicked button
+    const selectedBtn = document.getElementById(id);
+    selectedBtn.classList.add('btn-active');
+}
+
+allFilterBtn.addEventListener('click', function(){
+    toggleStyle('all-filter-btn');
+});
+interviewFilterBtn.addEventListener('click', function(){
+    toggleStyle('interview-filter-btn');
+});
+rejectedFilterBtn.addEventListener('click', function(){
+    toggleStyle('rejected-filter-btn');
+});
